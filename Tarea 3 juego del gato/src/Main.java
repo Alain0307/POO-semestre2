eb460f516b926aa546fac90617f37a84a7d4fab2
Alain0307 <113079684+Alain0307@users.noreply.github.com>
2023-02-22 18:40:49 -0600

    Perímetro del juego de gato

---
+++ Tarea 3 juego del gato/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        char hz = (char)95;
+        char vr = (char)124;
+
+        System.out.println(""+ hz + hz + hz + hz + hz);
+
+        for(int i=0; i<3; i++) {
+            System.out.println("" + vr + " " + " " + " " + "" + vr);
+        }
+
+        System.out.println(""+ hz + hz + hz + hz + hz);
+    }
+}
